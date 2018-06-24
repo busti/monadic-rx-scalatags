ac44dff8c5593c53b416dd6497dcde35058517cc
busti <mbust@live.de>
2018-06-25 00:51:49 +0200

    Fixed code not compiling.

--- src/main/scala/scalatags/mrx/nodes.scala
+++ src/main/scala/scalatags/mrx/nodes.scala
@@ -8,13 +8,19 @@ import scalatags.JsDom.all._
 class nodes {
   implicit class mRxStringFrag(rx: Rx[String]) extends Frag {
     def render: dom.Text = {
-      val node = dom.document.createTextNode(rx.)
+      val node = dom.document.createTextNode("")
       rx.impure.run(node.replaceData(0, node.length, _))
       node
     }
 
     def applyTo(t: Element) = {
       t.appendChild(render)
+    }
+  }
+
+  implicit class bindMRxElement[T <: dom.Element](rx: Rx[T]) extends Modifier {
+    def applyTo(t: Element) = {
+
     }
   }
 }
